b7be3e68fb32d46e720888525607055cb9aaf003
Anderson Ore Aleman <u202011467@upc.edu.pe>
2023-09-22 10:16:44 -0500

    azure deployment

--- app/src/main/java/com/example/android/asimov2023/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/android/asimov2023/retrofit/RetrofitClient.kt
@@ -9,7 +9,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.18.5:8080/api/v1/"
+    private const val BASE_URL = "https://asimov-api.azurewebsites.net/api/v1/"
 
 
     private val retrofit = Retrofit.Builder()
